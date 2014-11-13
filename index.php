3a455330140f925ff26deeb6a31c6b66d338b1a0
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-13 17:55:09 +0100

    vygenerovani kompilovaneho kodu

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
 require 'sandbox/vendor/autoload.php';
+
+$latte = new Latte\Engine;
+$latte->setLoader(new Latte\Loaders\StringLoader);
+
+echo $latte->compile('
+	<p>{$var}</p>
+');
+
+$latte->render('
+	<p>{$var}</p>
+
+', array(
+	'var' => 123,
+));
