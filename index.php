10f76c8b743411a70fb47c5e337f0a488730412a
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-13 18:14:39 +0100

    vytvoreni makra bold s parametrem 123 a filtrem trim

--- index.php
+++ index.php
@@ -2,16 +2,44 @@
 
 require 'sandbox/vendor/autoload.php';
 
+Tracy\Debugger::enable(FALSE);
+
 $latte = new Latte\Engine;
 $latte->setLoader(new Latte\Loaders\StringLoader);
 
+class BoldMacro implements Latte\IMacro
+{
+	function initialize() {}
+
+	function finalize() {}
+
+	function nodeOpened(Latte\MacroNode $node)
+	{
+		dump($node);
+	}
+
+	function nodeClosed(Latte\MacroNode $node)
+	{
+		dump($node);
+	}
+
+}
+
+$latte->onCompile[] = function($latte) {
+	$latte->addMacro('bold', new BoldMacro);
+	//$latte->addMacro('bold', new BoldMacro2);
+};
+
 echo $latte->compile('
-	<p>{$var}</p>
+	{bold 123 |trim}{$var}{/bold}
 ');
 
+
+/*
 $latte->render('
 	<p>{$var}</p>
 
 ', array(
 	'var' => 123,
 ));
+*/
