b248fbbc14a9ab48ee303c69aaecb320ab6b364a
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-13 16:40:35 +0100

    validace parametru, pouze v 2.3 dev

--- sandbox/app/PresenterExtension.php
+++ sandbox/app/PresenterExtension.php
@@ -2,11 +2,17 @@
 
 class PresenterExtension extends Nette\DI\CompilerExtension
 {
+	private $defaults = array(
+		'data' => NULL,
+		'hello' => '',
+	);
 
 	function loadConfiguration()
 	{
-		$config = $this->getConfig();
-		dump($config);
+		$config = $this->getConfig($this->defaults);
+		//dump($config);
+
+		$this->validateConfig($this->defaults);
 	}
 
 }
