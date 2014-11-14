771f54a8afdf78cad90b88b70d0321c01bc5bdca
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-14 14:32:08 +0100

    interni reprezentace prvku formulare

--- sandbox/app/controls/DateInput/DateInput.php
+++ sandbox/app/controls/DateInput/DateInput.php
@@ -2,6 +2,26 @@
 
 class DateInput extends Nette\Forms\Controls\BaseControl
 {
+	private $day, $month, $year;
 
+	function setValue($val = NULL)
+	{
+		if (!$val instanceof DateTime && $val !== NULL) {
+			throw new InvalidArgumentException;
+		}
+		if ($val === NULL) {
+			$this->day = $this->month = $this->year = NULL;
+		} else {
+			$this->day = $val->format('j');
+			$this->month = $val->format('n');
+			$this->year = $val->format('Y');
+		}
+	}
+
+
+	function getValue()
+	{
+		return clone $this->val;
+	}
 
 }
