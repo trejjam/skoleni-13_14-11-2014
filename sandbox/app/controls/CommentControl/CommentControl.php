f0ff4d343fa7e7e8b48d5f9afa80ad867d02baec
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-14 11:36:08 +0100

    registrace akce pomoci callbacku

--- sandbox/app/controls/CommentControl/CommentControl.php
+++ sandbox/app/controls/CommentControl/CommentControl.php
@@ -2,6 +2,8 @@
 
 class CommentControl extends Nette\Application\UI\Control
 {
+	public $onDelete;
+
 	/*function __construct($id)
 	{
 
@@ -10,7 +12,14 @@ class CommentControl extends Nette\Application\UI\Control
 	function render($id)
 	{
 		$this->template->getLatte()->setLoader(new \Latte\Loaders\StringLoader);
-		$this->template->render('<p>KOMENTAR {$id}</p>', ['id' => $id]);
+		$this->template->render('
+		<p>KOMENTAR {$id} <a n:href="delete $id">smazat</a></p>
+		', ['id' => $id]);
+	}
+
+	function handleDelete($id)
+	{
+		$this->onDelete($this, $id);
 	}
 
 }
--- sandbox/app/presenters/HomepagePresenter.php
+++ sandbox/app/presenters/HomepagePresenter.php
@@ -32,7 +32,11 @@ class HomepagePresenter extends BasePresenter
 
 	function createComponentComment()
 	{
-		return new \CommentControl;
+		$comment = new \CommentControl;
+		$comment->onDelete[] = function() {
+			$this->redirect('default');
+		};
+		return $comment;
 	}
 
 }
