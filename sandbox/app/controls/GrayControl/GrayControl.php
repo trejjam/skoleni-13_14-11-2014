8ceade0cc4060512e50f8d17dd328aae6dfa67e6
Jan Trejbal <jan.trejbal@gmail.com>
2014-11-14 10:43:27 +0100

    pristup k parametrum komponenty (render koment), osetreni parametru loadState

--- sandbox/app/controls/GrayControl/GrayControl.php
+++ sandbox/app/controls/GrayControl/GrayControl.php
@@ -5,8 +5,16 @@ class GrayControl extends Nette\Application\UI\Control
 	/** @persistent */
 	public $level = 50;
 
+	function loadState(array $params)
+	{
+		parent::loadState($params);
+		$this->level = min(max($this->level, 0), 255);
+	}
+
 	function render()
 	{
+		//$this->getParameter('level');
+		// $this->getParameter('val)
 		$this->template->render(__DIR__ . '/template.latte', array(
 			'level' => $this->level,
 		));
